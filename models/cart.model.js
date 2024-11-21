07361d6a4e355a85a8b34eec179343e1dc07baea
malakkorashy <mrvda225@gmail.com>
2024-11-21 17:27:12 +0200

    Update cart.model.js

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,12 +1,13 @@
-const cartItems = []; 
-class CartItem {
-  constructor(id, name, price, quantity, author, genre) {
-    this.id = id;      
-    this.name = name;    
-    this.price = price;
-    this.quantity = quantity;
-    this.author = author; 
-    this.genre = genre;   
-  }
-}
-module.exports = { cartItems, CartItem };
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  user: { type: String, required: true }, 
+  items: [
+    {
+      book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
+      quantity: { type: Number, required: true }
+    }
+  ]
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
